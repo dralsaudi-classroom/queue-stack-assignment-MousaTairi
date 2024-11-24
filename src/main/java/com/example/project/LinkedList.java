a835051500853ebc79a4673f2a1a394d3253828b
MousaTairi <444105322@student.ksu.edu.sa>
2024-11-24 16:39:02 +0300

    Update LinkedList.java

--- src/main/java/com/example/project/LinkedList.java
+++ src/main/java/com/example/project/LinkedList.java
@@ -3,7 +3,7 @@ package com.example.project;
 public class LinkedList<T> implements List<T>{
 	private Node<T> head;
 	private Node<T> current;
-	
+
     public LinkedList() {
 		head = current = null;
 	}
@@ -55,12 +55,33 @@ public class LinkedList<T> implements List<T>{
         }
     }
     public T mostFrequentElement() {
-        throw new UnsupportedOperationException("Not supported yet.");
-        //         Write the method mostFrequentElement, member of the class LinkedList, that returns
-        // the most frequent element in the list. The most frequent element is the element
-        // appearing the highest number of times. If one or more element appear the same
-        // number of times, the one encountered earlier is returned.
-        // Example 1.1. Given the list l : A, B, C, B, C, D, E, mostFrequentElement() returns
-        // B.
+    	if(head==null)
+    		return null ;
+    	current=head;
+    	int size=1;
+        while(current.next!=null) {
+        	current=current.next;
+        	size++;
+        }
+        T[] arr=(T[])new Object[size];
+        int[] num= new int[size];
+        
+        current=head;
+        for(int i=0;i<size;i++) {
+        	arr[i]=current.data;
+        	current=current.next;
+        	
+        }
+        for(int i=0;i<size;i++) {
+        	int x=2;
+        	for (int j=i+1;j<size;j++)
+        		if (arr[i].equals(arr[j]))
+        			num[i]=x++;
+        }
+        int highIndex=0;
+        for(int i=0;i<size;i++)
+        	if(num[i]>num[highIndex])
+        		highIndex=i;
+        return arr[highIndex];
     }
 }
